34de79d66731c90d90503dabbf5539560c78f7c2
artikgak <artem.hak@ukma.edu.ua>
2020-11-08 17:25:33 +0200

    [FrontEnd] normal front

--- ATM_Project/mainwindow.cpp
+++ ATM_Project/mainwindow.cpp
@@ -1,12 +1,16 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+const int MAINWINW = 500;
+const int MAINWINH = 550;
+const int GRIDBUTW = 150;
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
+    MainWindow::setGeometry(330,200,MAINWINW,MAINWINH);
 }
 
 MainWindow::~MainWindow()
