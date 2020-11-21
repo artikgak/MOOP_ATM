46fa0bcaec4587c321a3fbb28f2393bdb74ce1bf
artikgak <artem.hak@ukma.edu.ua>
2020-11-21 03:06:18 +0200

    [Tests] basic tests work

--- ATM_Project/testfront.cpp
+++ ATM_Project/testfront.cpp
@@ -1,20 +1,14 @@
 #include "testfront.h"
-
+#include "Utils.h"
 
 Test_Front::Test_Front(): atm(new ATM()), mw(new MainWindow(atm))
 {
 
 }
 
-void Test_Front::enterValidCardNum()
-{
-
-}
-
 void Test_Front::initTestCase() //will be called before the first test function is executed.
 {
-    atm->card = new Card("12345678");
-    mw->goToPage(EnterPIN);
+    atm->card = new Card("1234123412341234");
 }
 
 void Test_Front::cleanupTestCase() //will be called after the last test function was executed.
@@ -25,26 +19,35 @@ void Test_Front::cleanupTestCase() //will be called after the last test function
 
 void Test_Front::init() //will be called before each test function is executed.
 {
-
+    mw->goToPage(EnterPIN);
 }
 
-void Test_Front::clean() //will be called after every test function.
+void Test_Front::cleanup() //will be called after every test function.
 {
-mw->goToPage(EnterPIN);
+    delete atm->pin;
+    atm->pin = nullptr;
 }
 
 void Test_Front::testInputPIN()
 {
     mw->goToPage(EnterPIN);
-    mw->ui->button0->click();
     mw->ui->button1->click();
     mw->ui->button2->click();
     mw->ui->button3->click();
     mw->ui->button4->click();
-    mw->ui->button5->click();
-    mw->ui->button6->click();
-    mw->ui->button7->click();
-    mw->ui->button8->click();
-    mw->ui->button9->click();
-    QVERIFY(mw->ui->pinField->text()=="********");
+    QVERIFY(mw->ui->pinField->text().length()==4);
+    mw->ui->buttonL4->click();
+    QVERIFY(*(atm->pin)=="1234");  // is it correct that pin is saved at this point?
+    QVERIFY(mw->ui->stackedWidget->currentIndex()==Menu);
+}
+
+void Test_Front::testWrongPIN()
+{
+    mw->goToPage(EnterPIN);
+    mw->ui->button1->click();
+    mw->ui->button2->click();
+    QVERIFY(mw->ui->pinField->text().length()==2);
+    mw->ui->buttonL4->click();
+    QVERIFY(mw->ui->stackedWidget->currentIndex()==EnterPIN);
+    QVERIFY(mw->ui->wrongPINLabel->text()!="");
 }
